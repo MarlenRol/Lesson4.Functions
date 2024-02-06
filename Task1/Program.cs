9b6c431768c77c364c8c2685741d609b831e3248
Marlen <marlen.riskulov2014@yandex.ru>
2024-02-06 17:44:57 +0300

    Уроки семина

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.Write("Hello world");
+﻿
+Console.Write("Введите первое число: ");
+Int32 number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+Int32 number2 = Convert.ToInt32(Console.ReadLine());
+
+if ((number1 / number2) == number2)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
