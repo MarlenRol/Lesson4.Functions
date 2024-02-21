b57bdb1c27ecf154d483c22980aea438651e8053
Marlen <marlen.riskulov2014@yandex.ru>
2024-02-21 13:17:09 +0300

    Реализация первой задачи

---
+++ Task/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿while (true) 
+{
+    Console.Write("Введите число или 'q' для выхода: ");
+    string input = Console.ReadLine(); 
+    if (input == "q") 
+    {
+        break;
+    }
+
+    int number;
+    if (int.TryParse(input, out number)) 
+    {
+        int sum = 0;
+        while (number > 0) 
+        {
+            sum += number % 10; 
+            number /= 10; 
+        }
+        if (sum % 2 == 0) 
+        {
+            Console.WriteLine("СТОП");
+            break;
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое числоили 'q'.");
+    }
+
+}
+
