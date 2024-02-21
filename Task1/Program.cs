3a0a77071ee2fd891fec5e6437b8ab725704ba2c
Marlen <marlen.riskulov2014@yandex.ru>
2024-02-21 13:28:45 +0300

    Удаление лишнего

--- Task1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Задание 1
-// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
-Console.Write("Введите число:");
-int chislo = Convert.ToInt32(Console.ReadLine());
-if(chislo%7==0 && chislo%23==0){
-Console.WriteLine("Да");
-}else{
-    Console.WriteLine("нет");
-}
